9a2a758dfc341c01212bba65a18dc9000f3f2e87
ShubhamKhanna <shubham.khanna@synechron.com>
2012-07-04 15:57:31 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialNw::Application.config.secret_token = 'fa9e91e14bb2e0273bc2d9242acb87e24d9a922d7d7e0486db1f9a87e3b3a4e7042162edf8418f65a32b8d1738e564ddb1046e5064c4c1eef23d8df48f18b23e'
